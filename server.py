3820c8addc3665d3016e7b9e744c2a72b435466c
Lucas Antunes <cleiton.lucas416@gmail.com>
2024-09-21 16:51:56 -0300

    Criando rotas e métodos

--- server.py
+++ server.py
@@ -21,10 +21,29 @@ def get_empregados_cargo(cargo):
     
     out_empregados = []
     for empregado in empregados:
-        if cargo == empregado['cargo']:
+        if cargo == empregado['cargo'].lower():
             out_empregados.append(empregado)
     
     return out_empregados
+
+@app.route("/empregados/<info>/<value>")
+def get_empregados_info(info, value):
+    
+    out_empregados = []
+    for empregado in empregados:
+        if info in empregado.keys():
+            value_empregado = empregado[info]
+            
+            if type(value_empregado) == str:
+                if value == value_empregado.lower():
+                    out_empregados.append(empregado)
+            
+            if type(value_empregado) == int:
+                if int(value) == value_empregado:
+                    out_empregados.append(empregado)
+    
+    return out_empregados
+    
     
 
 if __name__ == "__main__":
